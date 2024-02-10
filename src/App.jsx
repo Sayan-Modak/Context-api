8b84f69d0d081312a20a80d65f3aa3447a50c8f9
Sayan Modak <sayanprodev111@gmail.com>
2024-02-10 18:40:37 +0530

    Normalize line endings

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>Hello Everyone</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
